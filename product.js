b1a091d7c5e6dca3eb3fdaf5edf88995f0b032e7
biklene <sint1532@gmail.com>
2026-07-08 11:50:49 +0300

    Create product.js

---
+++ product.js
@@ -0,0 +1,63 @@
+const params = new URLSearchParams(window.location.search);
+
+const productId = Number(params.get("id"));
+
+
+fetch("products.json")
+.then(response => response.json())
+.then(products => {
+
+    const product = products.find(p => p.id === productId);
+
+    const container = document.getElementById("product-details");
+
+
+    if (!container) {
+        return;
+    }
+
+
+    if (!product) {
+
+        container.innerHTML = `
+            <h2>Produkts nav atrasts</h2>
+        `;
+
+        return;
+    }
+
+
+    container.innerHTML = `
+
+    <div class="single-product">
+
+        <img src="${product.image}" alt="${product.name}">
+
+
+        <div class="product-info">
+
+            <h1>${product.name}</h1>
+
+            <p>${product.description}</p>
+
+            <h2>${product.price}</h2>
+
+            <p>PV punkti: ${product.pv}</p>
+
+
+            <button class="order-button">
+                Pasūtīt
+            </button>
+
+        </div>
+
+    </div>
+
+    `;
+
+})
+.catch(error => {
+
+    console.error("Kļūda:", error);
+
+});
