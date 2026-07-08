0397fe5f46de9a58c9c3b9a2794c2ac90be6eb81
biklene <sint1532@gmail.com>
2026-07-08 10:55:49 +0300

    Update products.js

--- products.js
+++ products.js
@@ -1,34 +1,56 @@
+console.log("products.js darbojas");
+
+
 fetch("products.json")
 .then(response => response.json())
 .then(products => {
 
+    console.log("Produkti ielādēti:", products);
+
+
     const container = document.getElementById("products-container");
 
+
     if (!container) {
+
+        console.log("Nav atrasts products-container");
+
         return;
     }
 
+
     products.forEach(product => {
+
 
         container.innerHTML += `
 
         <div class="product-card">
 
+
             <img src="${product.image}" alt="${product.name}">
+
 
             <h3>${product.name}</h3>
 
+
             <p>${product.description}</p>
+
 
             <strong>${product.price}</strong>
 
+
         </div>
+
 
         `;
 
+
     });
+
 
 })
 .catch(error => {
-    console.error("Kļūda ielādējot produktus:", error);
+
+    console.error("Kļūda:", error);
+
 });
